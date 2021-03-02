c39b23a610a7b8bbb56f642b47ddb94588ea67f4
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-03-02 15:33:04 -0500

    commit tree 55251437024ec958521ca5c7c91ff681377a2d11

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,16 @@
-// Code SimpleComponent Here
+import React, { Component } from "react";
+
+export default class SimpleComponent extends Component {
+  state = {
+    mood: "happy",
+  };
+
+  handleClick = () => {
+    this.setState((prevState) => {
+      return { mood: prevState.mood === "happy" ? "sad" : "happy" };
+    });
+  };
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>;
+  }
+}
